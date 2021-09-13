64e2c1ef932e97eb7f75dd3d6ad7d5c0fe5785ca
LoveAndCode <practice1356@gmail.com>
2021-09-13 20:49:45 +0900

    feat: backjoon problem

---
+++ java/src/algorithm/implement/Dog.java
@@ -0,0 +1,12 @@
+package algorithm.implement;
+
+public class Dog {
+    public static void main(String[] args) {
+     String dog = "|\\_/|\n" +
+             "|q p|   /}\n" +
+             "( 0 )\"\"\"\\\n" +
+             "|\"^\"`    |\n" +
+             "||_/=\\\\__|";
+        System.out.println(dog);
+    }
+}
---
+++ java/src/algorithm/implement/Rabbit.java
@@ -0,0 +1,11 @@
+package algorithm.implement;
+
+public class Rabbit {
+    //33309713
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
